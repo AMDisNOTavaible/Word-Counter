85380de575e6cfc706d913e01a103fad89fb1aa7
AMDisNOTavailable <161005519+AMDisNOTavaible@users.noreply.github.com>
2024-10-22 20:09:16 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,18 +1,27 @@
 using System;
-using UniqueWordCounterApp.Services;
+using WordCounterApp.Services;
 
-namespace UniqueWordCounterApp
+namespace WordCounterApp
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите путь до созданного файла и для создания файла.");
-            string inputPath = Console.ReadLine();
-            string outputPath = Console.ReadLine();
+            try
+            {
+                Console.WriteLine("Введите путь до созданного файла и для создания файла.");
+                string inputPath = Console.ReadLine();
+                string outputPath = Console.ReadLine();
 
-            FilePath filePath = new FilePath();
-            filePath.ProcessFiles(inputPath, outputPath);
+                FilePath filePath = new FilePath();
+                filePath.ProcessFiles(inputPath, outputPath);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Произошла ошибка: {ex.Message}");
+            }
+
+            Console.ReadKey();
         }
     }
 }
