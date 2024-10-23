563c5e27ea5f3eacbbcc86b46872988da0da28cb
AMDisNOTavailable <161005519+AMDisNOTavaible@users.noreply.github.com>
2024-10-23 18:26:10 +0300

    Update FileReader.cs

--- FileReader.cs
+++ FileReader.cs
@@ -3,7 +3,7 @@ using System.IO;
 using System.Linq;
 using System.Collections.Generic;
 
-namespace UniqueWordCounterApp.Services
+namespace WordCounterApp.Services
 {
     public class FileReader
     {
